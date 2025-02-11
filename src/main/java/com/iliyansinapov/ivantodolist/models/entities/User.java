f728fc1580da1ddc76c70d2e9e41559e97872c5f
IliyanSinapov <iliyan.sinapov@gmail.com>
2025-02-11 20:10:12 +0200

    daoidaw

---
+++ src/main/java/com/iliyansinapov/ivantodolist/models/entities/User.java
@@ -0,0 +1,20 @@
+package com.iliyansinapov.ivantodolist.models.entities;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "users")
+public class User {
+	@Id
+	@GeneratedValue(generator = "uuid")
+	@GenericGenerator(name = "uuid", strategy = "uuid2")
+	private String id;
+
+	@Column(nullable = false)
+	private String firstName;
+}
