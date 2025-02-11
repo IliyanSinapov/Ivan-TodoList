6089cb883b8568144020f9a09c91c0122f13f5ef
IliyanSinapov <iliyan.sinapov@gmail.com>
2025-02-11 10:09:40 +0200

    Initial commit

---
+++ src/main/java/com/iliyansinapov/ivantodolist/IvanTodoListApplication.java
@@ -0,0 +1,13 @@
+package com.iliyansinapov.ivantodolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IvanTodoListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IvanTodoListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/iliyansinapov/ivantodolist/IvanTodoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iliyansinapov.ivantodolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IvanTodoListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
